37c4c4fdce8f74cba0640d20c7115c69838064b8
Euvill <44217175+Euvill@users.noreply.github.com>
2022-04-25 20:32:27 +0800

    master

--- src/KLTtracker_app.cpp
+++ src/KLTtracker_app.cpp
@@ -2,17 +2,28 @@
 #include <pangolin/pangolin.h>
 #include <librealsense2/rs.hpp>
 #include <opencv2/opencv.hpp>
-
-using namespace std;
-using namespace cv;
-using namespace pangolin;
+#include <thread>
+#include <deque>
+#include <mutex>
+#include <unistd.h>
+#include <condition_variable>
 
 #define width 640 
 #define height 480 
 #define fps 30
 
 unsigned int d435_exp  = 0;
-unsigned int d435_gain = 0;
+
+static const std::string window_name = "KLTtracker";
+std::deque<cv::Mat> right_imgs;
+std::deque<cv::Mat> left_imgs;
+
+std::mutex mut_;
+std::condition_variable data_cond_;
+
+rs2::frameset frames;
+rs2::config cfg;
+rs2::pipeline pipe_;
 
 void setImageData(unsigned char * imageArray, cv::Mat image) {
     int clos = image.cols;
@@ -56,36 +67,17 @@ void d435_stereo_exp(void) {
     }
 }
 
-void d435_stereo_gain(void) {
-    rs2::context ctx;
-	auto list = ctx.query_devices(); 
-    rs2::device dev = list.front();
-    auto depth_stereo_sensor = dev.first<rs2::depth_stereo_sensor>();
-    if (depth_stereo_sensor.supports(RS2_OPTION_GAIN)) {
-        depth_stereo_sensor.set_option(RS2_OPTION_GAIN, d435_gain);
-    }
+void Setup() {
+    pangolin::CreateWindowAndBind(window_name, width * 2, height);
+
+    glEnable(GL_DEPTH_TEST);
+
+    pangolin::GetBoundWindow()->RemoveCurrent();
 }
 
-int main(int argc, char** argv) try {
+void Pangolin_Run() {
+    pangolin::BindToContext(window_name);
 
-	rs2::context ctx;
-	auto list = ctx.query_devices(); 
-	if (list.size() == 0) 
-		throw std::runtime_error("No device detected. Is it plugged in?");
-
-    rs2::device dev = list.front();
-    auto depth_sensor = dev.first<rs2::depth_sensor>();
-    depth_sensor.set_option(RS2_OPTION_EMITTER_ENABLED, 0.f);
-
-    rs2::frameset frames;
-    rs2::pipeline pipe;
-	rs2::config cfg;
-    cfg.enable_stream(RS2_STREAM_INFRARED, 1, width, height, RS2_FORMAT_Y8, fps);
-	cfg.enable_stream(RS2_STREAM_INFRARED, 2, width, height, RS2_FORMAT_Y8, fps);
-
-    pipe.start(cfg);
-    pangolin::CreateWindowAndBind("Pangolin", width * 2, height);
-    
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -99,9 +91,6 @@ int main(int argc, char** argv) try {
                               .AddDisplay(d_image1)
                               .AddDisplay(d_image2); 
 
-    unsigned char* imageArray1 = new unsigned char[width * height];
-    unsigned char* imageArray2 = new unsigned char[width * height];
-
     pangolin::GlTexture imageTexture1(width, height);
     pangolin::GlTexture imageTexture2(width, height);
 
@@ -109,43 +98,91 @@ int main(int argc, char** argv) try {
 
     pangolin::Var<unsigned int> d435_exposure("set. ", 1, 10000, 165000);
     pangolin::Var<std::function<void(void)>> exposure("set.Exposure", d435_stereo_exp);
-
-    pangolin::Var<unsigned int> d435_gain("set.  ", 16, 16, 248);
-    pangolin::Var<std::function<void(void)>> gain("set.Gain", d435_stereo_gain);
     
     pangolin::Var<std::function<void(void)>> laser_off("set.Laser Off", d435_laser_off);
     pangolin::Var<std::function<void(void)>> laser_on("set.Laser ON", d435_laser_on);
 
-    while(1) {
-        frames = pipe.wait_for_frames();
+    unsigned char* imageArray1 = new unsigned char[width * height];
+    unsigned char* imageArray2 = new unsigned char[width * height];
 
-        rs2::video_frame ir_frame_left = frames.get_infrared_frame(1);
-        rs2::video_frame ir_frame_right = frames.get_infrared_frame(2);
+    while(true) {
 
-        Mat pic_right(Size(width,height), CV_8UC1, (void*)ir_frame_right.get_data());
-        Mat pic_left(Size(width,height), CV_8UC1, (void*)ir_frame_left.get_data());
+        std::unique_lock<std::mutex> lk(mut_);
+
+        data_cond_.wait(lk, []{return !right_imgs.empty() && !left_imgs.empty();});
+
+        cv::Mat image1_front = right_imgs.front();
+        cv::Mat image2_front = left_imgs.front();
+
+        right_imgs.pop_front();
+        left_imgs.pop_front();
+
+        lk.unlock();
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        Mat image_initial = pic_right;
-
         d_image1.Activate();
-        setImageData(imageArray1, image_initial);
+        setImageData(imageArray1, image1_front);
         imageTexture1.Upload(imageArray1, GL_LUMINANCE, GL_UNSIGNED_BYTE);
         imageTexture1.RenderToViewportFlipY();
-        
-        image_initial = pic_left;
 
         d_image2.Activate();
-        setImageData(imageArray2, image_initial);
+        setImageData(imageArray2, image2_front);
         imageTexture2.Upload(imageArray2, GL_LUMINANCE, GL_UNSIGNED_BYTE);
         imageTexture2.RenderToViewportFlipY();
 
         d435_exp  = d435_exposure;
-        d435_gain = d435_gain;
-        
+     
         pangolin::FinishFrame();
     }
+
+    pangolin::GetBoundWindow()->RemoveCurrent();
+}
+
+void D435i_Run() {
+
+    while(true) {
+
+        frames = pipe_.wait_for_frames();
+
+        rs2::video_frame ir_frame_left = frames.get_infrared_frame(1);
+        rs2::video_frame ir_frame_right = frames.get_infrared_frame(2);
+
+        cv::Mat pic_right(cv::Size(width, height), CV_8UC1, (void*)ir_frame_right.get_data());
+        cv::Mat pic_left(cv::Size(width, height), CV_8UC1, (void*)ir_frame_left.get_data());
+
+        std::lock_guard<std::mutex> lk(mut_);
+        
+        right_imgs.push_back(pic_right);
+        left_imgs.push_back(pic_left);
+
+        data_cond_.notify_one();
+    }
+}
+
+int main(int argc, char** argv) try {
+
+	rs2::context ctx;
+	auto list = ctx.query_devices(); 
+	if (list.size() == 0) 
+		throw std::runtime_error("No device detected. Is it plugged in?");
+
+    rs2::device dev = list.front();
+    auto depth_sensor = dev.first<rs2::depth_sensor>();
+    depth_sensor.set_option(RS2_OPTION_EMITTER_ENABLED, 0.f);
+    
+    cfg.enable_stream(RS2_STREAM_INFRARED, 1, width, height, RS2_FORMAT_Y8, fps);
+	cfg.enable_stream(RS2_STREAM_INFRARED, 2, width, height, RS2_FORMAT_Y8, fps);
+
+    pipe_.start(cfg);
+
+    Setup();
+
+    std::thread render_loop(Pangolin_Run);
+    std::thread d435i_loop(D435i_Run);
+
+    render_loop.join();
+    d435i_loop.join();
 
     return 0;
 }
